b86b6253dec1b92963022c7b56c3461d2b1c2dd8
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-04-29 15:58:16 +0100

    Rudimentary product detail page added

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -7,3 +7,7 @@ products_blueprint = Blueprint("products", __name__)
 @products_blueprint.route("/products")
 def list_products():
     return render_template("products/index.html", products = product_repository.select_all())
+
+@products_blueprint.route("/products/<id>", methods = ['GET'])
+def product_detail(id):
+    return render_template("products/detail.html", product = product_repository.select(id))
